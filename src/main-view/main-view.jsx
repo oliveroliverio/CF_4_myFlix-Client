35440e81a4010ecac3444968ce884a2d3bd7fa07
oliveroliverio <oliveroliverio@gmail.com>
2024-10-04 16:58:46 -0700

    adding myComponent

--- src/main-view/main-view.jsx
+++ src/main-view/main-view.jsx
@@ -10,4 +10,18 @@ export const MainView = () => {
             <button>Test button</button>
         </>
     )
-};
+}
+
+export const MyComponent = () => {
+    let name = "John";
+    let num = 5;
+    let obj = { x: 32, y: 64 };
+    return (
+        <div>
+            <p>Hello {name}!</p>
+            <p>{5 + num}</p>
+            <p>{Math.random()}</p>
+            <p>{obj.x}</p>
+        </div>
+    );
+}
